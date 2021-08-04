a094032aafeca896d62676e2ea7341c6764c297b
Neil Kuan <guan840912@gmail.com>
2021-08-04 10:44:04 +0800

    add aws sts route

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
+import botocore
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! Fucking day "
-
+    return "Hello, World!!!"
+@app.route("/whoami")
+def getsts():
+    try:
+        sts = boto3.client('sts')
+        response = sts.get_caller_identity()
+        response = response.get('Arn').split('/')[1]
+    except botocore.exceptions.ClientError as e:
+        response = e
+    except botocore.exceptions.ProfileNotFound as e:
+        response = e
+    return "Hello, World! You are {}".format(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=80)
